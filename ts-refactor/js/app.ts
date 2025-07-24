8b0161c39cee43b8fa62a9bc704899ef29be0f0d
amkodoo <amerd.dev@gmail.com>
2025-07-24 07:33:31 +0200

    introduces ts refactor

---
+++ ts-refactor/js/app.ts
@@ -0,0 +1,59 @@
+import View from "./view.js";
+import Store from "./store.js";
+import type { Player } from "./types";
+
+const players: Player[] = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store(`live-t3-storage-key`, players);
+
+  //current Tab state change
+  store.addEventListener("statechange", () => {
+    view.render(store.game, store.stats);
+  });
+
+  //other Tab state change
+  window.addEventListener("storage", () => {
+    view.render(store.game, store.stats);
+  });
+
+  //initial render
+  view.render(store.game, store.stats);
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    //Save a valid move
+    store.playerMove(+square.id);
+  });
+}
+
+window.addEventListener("load", init);
